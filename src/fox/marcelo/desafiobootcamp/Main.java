25368d75e90de45c82f07737d218dfc55b21d248
marcelofox4 <marcelofox4@hotmail.com>
2022-01-27 22:33:55 -0300

    Finalização do projeto

--- src/fox/marcelo/desafiobootcamp/Main.java
+++ src/fox/marcelo/desafiobootcamp/Main.java
@@ -1,12 +1,15 @@
 package fox.marcelo.desafiobootcamp;
 
+import fox.marcelo.desafiobootcamp.domain.Bootcamp;
 import fox.marcelo.desafiobootcamp.domain.Curso;
+import fox.marcelo.desafiobootcamp.domain.Dev;
 import fox.marcelo.desafiobootcamp.domain.Mentoria;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("Descrição do curso Java");
@@ -22,8 +25,23 @@ public class Main {
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Marcelo");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos " + dev1.getConteudosInscritos());
+
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Conteúdos Inscritos " + dev1.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos " + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularTotalXp());
+
     }
 }
